951ccbad13a4ca41470f8fcd8d6d6458526124cf
Alex Mwaura <alexmwaura36@gmail.com>
2022-10-03 15:51:02 +0300

    edited the price validation

--- app/models/restaurant_pizza.rb
+++ app/models/restaurant_pizza.rb
@@ -1,5 +1,5 @@
 class RestaurantPizza < ApplicationRecord
     belongs_to :restaurant
     belongs_to :pizza
-    validates :price, length: { in: 1..30 }
+    validates :price, presence: true, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 30 }
 end
